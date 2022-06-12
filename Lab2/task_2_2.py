1840c4fb7070fcd4b44a205d7383275e05b64dab
Nnerin <102464502+Nnerin@users.noreply.github.com>
2022-06-12 12:03:22 +0300

    Add files via upload

---
+++ Lab2/task_2_2.py
@@ -0,0 +1,46 @@
+# Дана квадратная матрица. Проверить, является ли она симметричной относительно главной диагонали.
+import random
+import numpy as np
+
+A = np.array([[2,1,5,-4], [1,6,3,-9], [5,3,8,7], [-4,-9,7,3]])
+print(A)
+print("----------")
+h=('')
+for k in range(len(A)):
+    for l in range(1,len(A)):
+        if A[k][l]!=A[l][k]:
+            h=('False')
+            break
+if h!=('False'):
+    print('Матрица симметрична\n')
+else:
+    print('Обычная матрица')
+
+A = np.array([[1,2,5,-4], [1,6,3,-9], [5,3,8,7], [-4,-9,7,3]])
+print(A)
+print("----------")
+h=('')
+for k in range(len(A)):
+    for l in range(1,len(A)):
+        if A[k][l]!=A[l][k]:
+            h=('False')
+            break
+if h!=('False'):
+    print('Матрица симметричная\n')
+else:
+    print('Матрица несимметричная')
+
+# n=int(input('Введите кол-во строк и столбцов матрицы: '))
+# h=('')
+# matrix=[[random.randrange(10) for i in range(n)] for j in range(n)]
+# for elem in matrix:
+#     print(elem)
+# for k in range(n):
+#     for l in range(1,n):
+#         if matrix[k][l]!=matrix[l][k]:
+#             h=('False')
+#             break
+# if h!=('False'):
+#     print('Матрица симметрична')
+# else:
+#     print('Обычная матрица')
